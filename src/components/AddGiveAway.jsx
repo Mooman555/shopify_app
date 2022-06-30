9ae1e55de9add51e843799ebaab9db979f9200c6
Mooman_Laptop\Mooman Butt <mooman@karigar.pk>
2022-06-30 19:52:40 +0500

    -- with child deployement

--- src/components/AddGiveAway.jsx
+++ src/components/AddGiveAway.jsx
@@ -396,12 +396,16 @@ export const AddGiveAway = ({ setToggle }) => {
 
               <br />
 
+              {/* eslint-disable   */}
+
               {showChild && formValues && (
                 <ChildFlashSale
                   formValues={formValues}
                   changeHandler={handleChangeFields}
                 />
               )}
+
+              {/* eslint-enable */}
             </Card>
           </FormLayout>
         </Layout.AnnotatedSection>
